487c810effc3b8682cd5f5bd9a5ecad85691a3cf
Nguyễn Đình Phúc <102985779+luckylife-2004@users.noreply.github.com>
2025-11-23 23:59:32 +0700

    feat: xóa file dư thừa

--- ${project.build.directory}/classes/db/migration/V4__Create_projects_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE projects (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, -- Unique identifier for each project
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    owner_id integer REFERENCES users(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Creation timestamp
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, -- Last update timestamp
-    deleted_at TIMESTAMP -- Deletion timestamp
-);
